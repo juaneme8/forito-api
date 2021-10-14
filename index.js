3e91f4e0914b702b2989b43ab9bf05de1b377abe
Juan Ocho <juaneme8@gmail.com>
2021-10-14 15:48:15 -0300

    Working with data of last 5 pages

--- index.js
+++ index.js
@@ -6,74 +6,59 @@ const app = express();
 
 const PORT = 3001;
 
-const posts = []
+const posts = [];
 // const baseURL = 'https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina?goto=newpost'
-const baseURL = 'https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina/page'
-
+const baseURL = 'https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina/page';
 
 const getLastPage = async () => {
-    try {
-        const response = await axios.get(`${baseURL}12648`)
-        const html = response.data;
-        const $ = cheerio.load(html, { decodeEntities: false });
+	try {
+		const response = await axios.get(`${baseURL}12648`);
+		const html = response.data;
+		const $ = cheerio.load(html, { decodeEntities: false });
 
-        const lastPageLink = $('.first_last').last().find("a").attr('href');
+		const lastPageLink = $('.first_last').last().find('a').attr('href');
 
-        // console.log(lastPageLink)
+		// console.log(lastPageLink)
 
-        return lastPageLink.split("page")[1].split('?')[0]
+		return lastPageLink.split('page')[1].split('?')[0];
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
-
-
-
-
-
-
+// [result1, result2] = Promise.all([async1(), async2()]);
 
 app.get('/', async (req, res) => {
-    const lastPage = await getLastPage()
-    // console.log(lastPage)
+	const lastPage = await getLastPage();
+	console.log(lastPage);
+	for (let i = 0; i < 6; i++) {
+		const currentPage = lastPage - i;
+		console.log({ currentPage });
 
-    for (let page = lastPage - 5; page = lastPage; page++) {
-        axios.get(`${baseURL}${lastPage}`)
-            .then(response => {
-                console.log('page',page)
+		await axios.get(`${baseURL}${currentPage}`).then(response => {
+			const html = response.data;
+			const $ = cheerio.load(html, { decodeEntities: false });
 
-                const html = response.data;
-                const $ = cheerio.load(html, { decodeEntities: false });
+			$('li.postbitlegacy').each((idx, item) => {
+				const date = $(item).find('.date').text();
+				const content = $(item).find('blockquote.postcontent').text();
+				const likes = $(item).find('.likes').text();
+				const link = 'https://foros.3dgames.com.ar/' + $(item).find('.postcounter').attr('href');
 
-                $("li.postbitlegacy").each((idx, item) => {
-
-
-                    const date = $(item).find('.date').text()
-                    const content = $(item).find('blockquote.postcontent').text()
-                    const likes = $(item).find('.likes').text()
-                    const link = 'https://foros.3dgames.com.ar/' + $(item).find('.postcounter').attr('href')
-
-                    // console.log(likes)
-                    if (likes > 0) {
-                        posts.push({ page, date, content, likes, link })
-                    }
-                })
-
-
-            })
-    }
-    res.json(posts)
-
-})
+				// console.log(likes)
+				if (likes > 2) {
+					posts.push({ currentPage, date, content, likes, link });
+				}
+			});
+		});
+	}
+	res.json(posts);
+});
 
 app.get('/posts', (req, res) => {
-
-    // res.json(posts)
-
-})
+	// res.json(posts)
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}`)
+	console.log(`Listening on Port ${PORT}`);
 });
